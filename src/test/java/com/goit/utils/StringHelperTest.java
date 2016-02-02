bf0fe6cbe22fe377cfd6417781a1ac7a73465cf1
shved90 <shvechikov90@gmail.com>
2016-02-02 22:42:23 +0200

    Example of test

---
+++ src/test/java/com/goit/utils/StringHelperTest.java
@@ -0,0 +1,63 @@
+package com.goit.utils;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static com.goit.utils.StringHelper.*;
+import static org.junit.Assert.*;
+
+/**
+ * Created by home1 on 02.02.16.
+ */
+public class StringHelperTest {
+
+    private static final String TEST_WORD1 = "wen";
+    private static final String TEST_WORD2 = "wne";
+    private static final String WRONG_TEST_WORD1 = "123ASDAwen";
+    private static final String WRONG_TEST_WORD2 = "wen   ";
+
+
+    private List<String> words;
+
+    @Before
+    public void setUp() throws Exception {
+        words = Arrays.asList("done", "new", "node", "low", "wow", "awe", "wae");
+    }
+
+    @Test
+    public void testIsAnagram() throws Exception {
+        assertTrue(isAnagram(TEST_WORD1, TEST_WORD2));
+        assertFalse(isAnagram(TEST_WORD1, WRONG_TEST_WORD1));
+        assertFalse(isAnagram(TEST_WORD1, null));
+        assertFalse(isAnagram(null, TEST_WORD2));
+        assertFalse(isAnagram(TEST_WORD1, ""));
+        assertFalse(isAnagram("", TEST_WORD2));
+        assertFalse(isAnagram(null, null));
+        assertFalse(isAnagram("", ""));
+        assertFalse(isAnagram(TEST_WORD1, WRONG_TEST_WORD2));
+        assertFalse(isAnagram("", "        "));
+    }
+
+    @Test
+    public void testGetAnagram() throws Exception {
+
+    }
+
+    @Test
+    public void testGetAnagramPairsList() throws Exception {
+
+    }
+
+    @Test
+    public void testIsEmpty() throws Exception {
+
+    }
+
+    @Test
+    public void testIsEmpty1() throws Exception {
+
+    }
+}
